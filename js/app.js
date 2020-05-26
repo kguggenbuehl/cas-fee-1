df4ecafad3f9ce3f00ae1247a016d5faad7aeae5
Kaspar Guggenbühl <k.guggenbuehl@backslash.ch>
2020-05-26 22:50:10 +0200

    added functionality to sort-by-importance-button

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 const todos = [
-    {"id": "01", "title": "Lorem ipsum dolor sit amet", "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", "rating": 3, "finishdate": new Date("January 31 2020 12:30"),"finished": false},
-    {"id": "02", "title": "ipsum dolor sit ", "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", "rating": 2, "finishdate": new Date("January 10 2020 12:30"),"finished": false},
-    {"id": "03", "title": "Lorem  dolor amet", "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", "rating": 5, "finishdate": new Date("January 20 2020 12:30"),"finished": true},
+    {"id": "01", "title": "Lorem ipsum dolor sit amet", "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", "rating": 3, "finishdate": new Date("January 31 2020 12:30"),"finished": false, "createdate": new Date("January 31 2019 12:30")},
+    {"id": "02", "title": "ipsum dolor sit ", "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", "rating": 2, "finishdate": new Date("January 10 2020 12:30"),"finished": false, "createdate": new Date("January 20 2019 12:30")},
+    {"id": "03", "title": "Lorem  dolor amet", "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", "rating": 5, "finishdate": new Date("January 20 2020 12:30"),"finished": true, "createdate": new Date("January 10 2019 12:30")}
 ];
+
+function sortByRate(todo1, todo2){
+    return todo2.rating - todo1.rating;
+}
 
 function createTodoList(todos) {
 
@@ -38,9 +42,15 @@ function createTodoList(todos) {
 }
 
 function init() {
+    // insert todo-list into DOM
     document.getElementById('app__todos-lst').innerHTML = createTodoList(todos);
+    // add eventhandler to "Show finish"-Button
     document.getElementById('sort__input--show-finished').addEventListener('click', function(){
         document.getElementById('app__todos-lst').classList.toggle('app__todos-lst--hidefinished');
+    })
+    // add eventhandler to "Sort by importance"-Button
+    document.getElementById('sort__input--rating').addEventListener('click', function(){
+        document.getElementById('app__todos-lst').innerHTML = createTodoList(todos.sort(sortByRate));
     })
 }
 
