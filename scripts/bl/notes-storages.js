f9c1dc61f0c5943661e103e3ddad4388270a5c1f
Kaspar Guggenbühl <k.guggenbuehl@backslash.ch>
2020-05-29 14:25:55 +0200

    added sort-functions: to be refactoured

--- scripts/bl/notes-storages.js
+++ scripts/bl/notes-storages.js
@@ -5,14 +5,34 @@ export default function getNotes(orderBy, showFinished) {
     let allTodos = staticTodos();
 
     if (!showFinished){
-        allTodos = allTodos.filter((value) => !value.finished);
+        allTodos = filterNotFinished(allTodos);
+    }
+
+    if(orderBy === 1) {
+        allTodos = allTodos.sort(sortByFinishDate);
+    }
+    if(orderBy === 2) {
+        allTodos = allTodos.sort(sortByCreateDate);
+    }
+    if(orderBy === 3) {
+        allTodos = allTodos.sort(sortByRate);
     }
 
     return allTodos;
 }
 
-
+function filterNotFinished(allTodos){
+    return allTodos.filter((value) => !value.finished);
+}
 
 function sortByRate(todo1, todo2){
     return todo2.rating - todo1.rating;
+}
+
+function sortByFinishDate(todo1, todo2){
+    return todo2.finishdate - todo1.finishdate;
+}
+
+function sortByCreateDate(todo1, todo2){
+    return todo2.createdate - todo1.createdate;
 }
