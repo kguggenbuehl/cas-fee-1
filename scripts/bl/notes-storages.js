6eaa06d953775acdc098989d62091e501a723446
Kaspar Guggenbühl <k.guggenbuehl@backslash.ch>
2020-05-29 11:27:28 +0200

    changed show-finished-functionality. update dom instead changing classes

--- scripts/bl/notes-storages.js
+++ scripts/bl/notes-storages.js
@@ -1,3 +1,18 @@
+import staticTodos from '../dl/data.js';
+
+export default function getNotes(orderBy, showFinished) {
+
+    let allTodos = staticTodos();
+
+    if (!showFinished){
+        allTodos = allTodos.filter((value) => !value.finished);
+    }
+
+    return allTodos;
+}
+
+
+
 function sortByRate(todo1, todo2){
     return todo2.rating - todo1.rating;
 }
--- scripts/ui/app-controller.js
+++ scripts/ui/app-controller.js
@@ -1,22 +1,30 @@
-import todos from '../dl/data.js';
+import getNotes from '../bl/notes-storages.js';
+
+let templateContainer;
+let templateSource;
+let createTodoList;
 
 function initEventHandlers() {
 
-    const templateContainer = document.getElementById('app__todos');
-    const templateSource = document.getElementById('template-todo-item').innerHTML;
-    const createTodoList = Handlebars.compile(templateSource);
+    templateContainer = document.getElementById('app__todos');
+    templateSource = document.getElementById('template-todo-item').innerHTML;
+    createTodoList = Handlebars.compile(templateSource);
 
-    templateContainer.innerHTML = createTodoList(todos);
+    renderTodoList(1, false);
 
     // add eventhandler to "Show finish"-Button
-    document.getElementById('sort__input--show-finished').addEventListener('click', function(){
-        document.getElementById('app__todos-lst').classList.toggle('app__todos-lst--hidefinished');
+    document.getElementById('sort__input--show-finished').addEventListener('click', function(event){
+        renderTodoList(1, event.target.checked);
     })
     // add eventhandler to "Sort by importance"-Button
     document.getElementById('sort__input--rating').addEventListener('click', function(){
         document.getElementById('app__todos-lst').innerHTML = createTodoList(todos.sort(sortByRate));
     })
 
+}
+
+function renderTodoList(sortBy, showFinished){
+    templateContainer.innerHTML = createTodoList(getNotes(sortBy, showFinished));
 }
 
 // wait until scripts have been loaded
