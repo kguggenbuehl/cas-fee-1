a5e2f64cc8ae335c3c2b57e0e3a442ad0396d7b6
Kaspar Guggenbühl <k.guggenbuehl@backslash.ch>
2020-06-25 22:29:24 +0200

    better cookie class

--- public/scripts/bl/cookie-service.js
+++ public/scripts/bl/cookie-service.js
@@ -1,23 +1,22 @@
 class CookieService {
     setCookie(cname, cvalue, exdays) {
-        let d = new Date();
-        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-        let expires = "expires="+d.toUTCString();
-        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+        let expires = "";
+        if (exdays) {
+            let date = new Date();
+            date.setTime(date.getTime()+(exdays*24*60*60*1000));
+            expires = "; expires="+date.toUTCString();
+        }
+        document.cookie = cname+"="+cvalue+expires+"; path=/";
     }
     getCookie(cname) {
-        let name = cname + "=";
+        const nameEQ = cname + "=";
         let ca = document.cookie.split(';');
-        for (let i = 0; i < ca.length; i++) {
+        for(let i=0;i < ca.length;i++) {
             let c = ca[i];
-            while (c.charAt(0) == ' ') {
-                c = c.substring(1);
-            }
-            if (c.indexOf(name) == 0) {
-                return c.substring(name.length, c.length);
-            }
+            while (c.charAt(0)===' ') c = c.substring(1,c.length);
+            if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length,c.length);
         }
-        return false;
+        return null;
     }
 }
 
